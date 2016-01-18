4ecd16221c9bd674d4dbb458514e4c11208725c3
Case Rosenfelt <rosenfelt@wisc.edu>
2016-01-18 16:07:22 -0700

    Finishing Streetfighter Project

--- js/app.js
+++ js/app.js
@@ -8,12 +8,34 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show();
+      playHadouken();
+      $('.ryu-ready').hide();
+  	  $('.ryu-throwing').show();
+  	  $('.hadouken').finish().show().animate(
+          {'left': '1020px'},
+   		  500,
+   	 	  function() {    
+    		  $(this).hide();
+    		  $(this).css('left', '520px');
+           });
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+  $(document).keydown(function(e) {
+    if(e.which == 88) {
+    	$('.ryu-ready').hide();
+    	$('.ryu-cool').show();
+    }
+  });
+  $(document).keyup(function(e) {
+    	$('.ryu-cool').hide();
+    	$('.ryu-ready').show();
+  });
+  function playHadouken () {
+   $('#hadouken-sound')[0].volume = 0.5;
+   $('#hadouken-sound')[0].load();
+   $('#hadouken-sound')[0].play();
+  }
 });
